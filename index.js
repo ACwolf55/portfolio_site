03503dc522042b16b9c43489461b005bdaef0c04
Alex Crowe <alexcwolf5@gmail.com>
2021-06-07 18:31:34 -0700

    jquery not working on github pages fix attempt

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(()=>{
 
-    $(window).bind('scroll',function() {
+    $(window).bind("scroll",function() {
         if($(window).scrollTop() > 100) {
-            $('.doublearrows').fadeOut(500)
+            $(".doublearrows").fadeOut(500)
         } else {
-            $('.doublearrows').show()
+            $(".doublearrows").show()
         }
     })
 
